6c37e87ab9f235e93b9c482ef84d107f50ea095c
CarlosRoldon <recopilatortwitch@gmail.com>
2026-08-27 10:06:03 -0500

    Clase 27-08 Mostrar empleados

---
+++ Leo/clase 27-08/insertar_alu.php
@@ -0,0 +1,21 @@
+<?php 
+include('./connection_inc.php');
+$link = conectar();
+// traemos los datos enviados por el formulario de registro de alumnos
+$cod = $_POST['codigo'];
+$nom = $_POST['nombre'];
+$ape = $_POST['apellido'];
+$email = $_POST['email'];
+$tel = $_POST['telefono'];
+$fn = $_POST['fecha_nacimiento'];
+
+$sql = "INSERT INTO alumnos values ($cod, '$nom', '$ape', '$email', '$tel', '$fn')";
+
+// ejecutamos la consulta
+$res = mysqli_query($link, $sql) or die("Error al insertar el alumno: " . mysqli_error($link));
+
+echo "<h2>Alumno registrado correctamente</h2>" . "<a href='./regalu.php'>Volver</a>";
+
+// cerramos la conexion
+mysqli_close($link);
+?>
